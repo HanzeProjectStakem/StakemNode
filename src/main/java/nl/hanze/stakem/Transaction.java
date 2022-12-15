df273cf9973a832885964d6206a499d86c408430
Douwe <d.de.leeuw@st.hanze.nl>
2022-12-15 17:38:12 +0100

    created basic Transaction Class

--- src/main/java/nl/hanze/stakem/Transaction.java
+++ src/main/java/nl/hanze/stakem/Transaction.java
@@ -1,14 +1,62 @@
 package nl.hanze.stakem;
 
-public class Transaction {
-    private final String sender;
-    private final String receiver;
-    private final double amount;
-    private Long timeStamp;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
 
-    public Transaction(String sender, String receiver, double amount) {
+public class Transaction {
+
+    private final Integer version;
+    private final String sender;
+    private final String recipient;
+    private final Integer amount;
+    private final Long timeStamp;
+    private final Signature signature;
+    private final String hash;
+
+    public Transaction(Integer version, String sender, String recipient, Integer amount, Long timeStamp, Signature signature) {
+        this.version = version;
+        this.timeStamp = timeStamp;
+        this.signature = signature;
         this.sender = sender;
-        this.receiver = receiver;
+        this.recipient = recipient;
         this.amount = amount;
+        this.hash = generateHash();
+    }
+
+    public String getHash() {
+        return this.hash;
+    }
+
+    private String generateHash() {
+        String dataToHash = version
+                + sender
+                + recipient
+                + amount
+                + timeStamp
+                + signature;
+
+        byte[] bytes = null;
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA3-256");
+            bytes = digest.digest(dataToHash.getBytes(StandardCharsets.UTF_8));
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        assert bytes != null;
+        return bytesToHex(bytes);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte aByte : bytes) {
+            String hex = Integer.toHexString(0xff & aByte);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
